4b17c10ff806587eb843f61296faf10e4a42db6a
joeljoanlopez <joeljoanlopez@gmail.com>
2025-02-07 15:38:20 +0100

    Add AttackController.cs

--- Assets/Scripts/AttackController.cs
+++ Assets/Scripts/AttackController.cs
@@ -1,5 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+
 public class AttackController : MonoBehaviour
 {
-	
+	public Collider2D attackCollider;
+	public float attackTime = 0.5f;
+
+	private void OnAttack()
+	{
+		StartCoroutine(EnableAttackCollider());
+	}
+
+	private IEnumerator EnableAttackCollider()
+	{
+		attackCollider.enabled = true;
+		yield return new WaitForSeconds(attackTime);
+		attackCollider.enabled = false;
+	}
 }
