64dcaa23e9a7a26cce44f347719ebd98fd90c943
joeljoanlopez <joeljoanlopez@gmail.com>
2025-02-12 19:19:55 +0100

    Create JumpController.cs

---
+++ Assets/Scripts/JumpController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour
+{
+	public float jumpForce = 10f;
+	private Rigidbody2D rigidBody;
+
+	private void Start()
+	{
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	private void OnJump()
+	{
+		// Jump only if the player is on the ground
+		if (IsGrounded())
+		{
+			rigidBody.linearVelocity = Vector2.up * jumpForce * Time.deltaTime;
+		}
+	}
+
+	private bool IsGrounded()
+	{
+		return true;
+	}
+}
