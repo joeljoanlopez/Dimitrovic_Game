fe30f40ff55be0bf843e656c273371a5f9225ed0
joeljoanlopez <joeljoanlopez@gmail.com>
2025-02-21 16:14:48 +0100

    Added Heavy attack

--- Assets/Scripts/AttackController.cs
+++ Assets/Scripts/AttackController.cs
@@ -8,8 +8,13 @@ public class AttackController : MonoBehaviour
 
 	[Header("Light Attack")]
 	public float lightAttackDelay = 0.1f;
-	public float lightAttackTime = 0.3f;
+	public float lightAttackTime = 0.2f;
 	public float lightAttackDamage = 10f;
+	
+	[Header("Heavy Attack")]
+	public float heavyAttackDelay = 0.3f;
+	public float heavyAttackTime = 0.2f;
+	public float heavyAttackDamage = 20f;
 
 	private void Start()
 	{
@@ -20,6 +25,11 @@ public class AttackController : MonoBehaviour
 	private void OnLightAttack()
 	{
 		StartCoroutine(EnableAttackCollider(lightAttackDelay, lightAttackTime, lightAttackDamage));
+	}
+	
+	private void OnHeavyAttack()
+	{
+		StartCoroutine(EnableAttackCollider(heavyAttackDelay, heavyAttackTime, heavyAttackDamage));
 	}
 
 	private IEnumerator EnableAttackCollider(float attackDelay, float attackTime, float attackDamage)
