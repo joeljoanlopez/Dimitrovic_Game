c23a435ffa06153c391f7a0d8ad78b006d54d91a
joeljoanlopez <joeljoanlopez@gmail.com>
2025-02-23 14:08:34 +0100

    Create light attack and player health controller

---
+++ Assets/Scripts/EnemyAttackHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FSM_Behaviors;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public enum AttackType
+    {
+        Light,
+        Heavy,
+        Area,
+        Jump,
+    }
+    
+    public class EnemyAttackHandler : MonoBehaviour
+    {
+        public AttackType attackType;
+        public AI_Data aiData;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.CompareTag("Player")) return;
+            PlayerHealthController playerHealth = other.GetComponent<PlayerHealthController>();
+            playerHealth.TakeDamage(DecideDamage());
+        }
+        
+        private int DecideDamage()
+        {
+            switch (attackType)
+            {
+                case AttackType.Light:
+                    return aiData.lightAttackDamage;
+                // case AttackType.Heavy:
+                //     return aiData.heavyAttackDamage;
+                // case AttackType.Area:
+                //     return aiData.areaAttackDamage;
+                // case AttackType.Jump:
+                //     return aiData.jumpAttackDamage;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FSM_Behaviors/AI_Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace FSM_Behaviors
+{
+    public class AI_Data : MonoBehaviour
+    {
+        [Header ("Light attack")]
+        public Collider2D lightAttackCollider;
+        public float lightAttackDelay = 1f;
+        public float lightAttackTime = 0.2f;
+        public int lightAttackDamage = 10;
+
+        private void Start()
+        {
+            lightAttackCollider.enabled = false;
+        }
+    }
+}
