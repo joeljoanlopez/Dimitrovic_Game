a7ebaad2d1d1aec9d99a43d68278dc51ece15119
joeljoanlopez <joeljoanlopez@gmail.com>
2025-02-28 15:53:21 +0100

    Create cameraFollow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+namespace DefaultNamespace
+{
+	public class CameraController : MonoBehaviour
+	{
+		public Transform target;
+		public float smoothSpeed = 0.125f;
+		
+		private void Start()
+		{
+			if (target == null)
+			{
+				throw new Exception("Target is not set");
+			}
+		}
+		
+		private void Update()
+		{
+			Vector3 desiredPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
+			Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+			transform.position = smoothedPosition;
+		}
+		
+		public void SetTarget(Transform target)
+		{
+			this.target = target;
+		}
+	}
+}
